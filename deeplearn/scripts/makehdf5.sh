7db544f010302189b14596bbf082da317768ab44
rmovva <therajiv@gmail.com>
2017-06-26 11:49:15 -0700

    Added deep learning code and model configurations

---
+++ deeplearn/scripts/makehdf5.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+make_hdf5 --yaml_configs ../yamls/classificationJun22WithoutQc_makehdf5/* --output_dir ../hdf5files/classificationJun22WithoutQcV2/
---
+++ deeplearn/scripts/makehdf5_regression.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+make_hdf5 --yaml_configs ../yamls/regressionJun24Positives_makehdf5/* --output_dir ../hdf5files/regressionJun24Positives/
