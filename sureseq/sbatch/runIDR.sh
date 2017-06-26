f7fe55ab5faf99ac628a0a77087e27fa4ef3eaf4
rmovva <therajiv@gmail.com>
2017-06-26 12:27:38 -0700

    Added Jupyter notebook for data exploration and QC

---
+++ sureseq/sbatch/runIDR.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -n 4                           # Number of cores
+#SBATCH -N 1                           # Ensure that all cores are on one machine
+#SBATCH -t 48:00:00                    # Runtime in HH:MM:SS
+#SBATCH -p owners                      # Partition to submit to
+#SBATCH --mem=512G                     # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o IDRsbatchJune20_%j.out      # File to which STDOUT will be written
+#SBATCH -e IDRsbatchJune20_%j.err      # File to which STDERR will be written
+#SBATCH --mail-type=ALL                # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=therajiv@gmail.com # Email to which notifications will be sent
+
+idr --samples ../fixedIdrSureFragmentSignalMappedReplicate1.bed ../fixedIdrSureFragmentSignalMappedReplicate2.bed --input-file-type bed --rank 5 --plot -o idrSingleBpSbatchRun.txt
